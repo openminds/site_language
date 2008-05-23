71aa7ffcd86d388ce71d2efb2f573f54e3972c6d
Benny Degezelle <benny@gorilla-webdesign.be>
2008-05-23 18:00:48 +0800

    fixing routing for page & snippet translated edit url
    Signed-off-by: Jan De Poorter <jan@defv.be>

--- site_language_extension.rb
+++ site_language_extension.rb
@@ -1,3 +1,8 @@
+require_dependency 'application'
+
+include Globalize
+Locale.set_base_language(SiteLanguage.default)
+
 class SiteLanguageExtension < Radiant::Extension
   version "1.0"
   description "Habla Nederlands, sir? Si oder non?"
@@ -5,18 +10,19 @@ class SiteLanguageExtension < Radiant::Extension
   
   define_routes do |map|
     map.connect 'admin/site_language/:action', :controller => 'admin/site_languages'
+    map.translated_page_edit 'admin/pages/edit/:id/:language', :controller => 'admin/page', :action => 'edit'
+    map.translated_snippet_edit 'admin/snippets/edit/:id/:language', :controller => 'admin/snippet', :action => 'edit'
     begin
       SiteLanguage.codes.each do |code|
         langname = Locale.new(code).language.code
         map.connect "#{langname}/*url", :controller => 'site', :action => 'show_page', :language => code
       end
     rescue
-      # raise SiteLanguageError, "Migrations not ran yet.."
+      #raise SiteLanguageError, "Migrations not ran yet.."
     end
   end
 
   def activate
-    require_dependency 'application'
     admin.tabs.add "Languages", "/admin/site_language", :after => "Layouts", :visibility => [:admin, :developer]
     # We need globalize
     unless ActiveRecord::Base.respond_to? :translates
@@ -30,19 +36,34 @@ class SiteLanguageExtension < Radiant::Extension
     admin.tabs.remove "Languages"
   end
   
+  private
+  
   def enhance_classes
     # stuff is not automatically inherited by already loaded classes
-    ApplicationController.descendants.each do |controller|
-      controller.send(:include, SiteLanguage::ControllerExtensions)
+    Admin::PageController.class_eval do
+      before_filter :set_locale, :except => [:index]
+      def set_locale
+        Locale.set(params[:language] || SiteLanguage.default)
+      end
+      protected
+      def continue_url(options)
+        options[:redirect_to] || (params[:continue] ? translated_page_edit_url(:id => @page.id, :language => params[:language]) : page_index_url)
+      end
+    end
+    
+    Admin::SnippetController.class_eval do
+      before_filter :set_locale, :except => [:index]
+      def set_locale
+        Locale.set(params[:language] || SiteLanguage.default)
+      end
+      protected
+      def continue_url(options)
+        options[:redirect_to] || (params[:continue] ? translated_snippet_edit_url(:id => @snippet.id, :language => params[:language]) : snippet_index_url)
+      end
     end
 
     SiteController.send(:include, SiteLanguage::SiteControllerExtensions)
     Page.send :include, SiteLanguage::PageExtensions
-    
-    Object.class_eval do
-      include Globalize
-      Locale.set_base_language(SiteLanguage.default)
-    end
 
     PagePart.class_eval do
       translates :content
@@ -52,8 +73,4 @@ class SiteLanguageExtension < Radiant::Extension
       translates :content
     end
   end
-end
-
-
-class SiteLanguageError < StandardError
 end
