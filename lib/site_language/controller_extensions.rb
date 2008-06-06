59d6526c5334d74ef08048dfda7ab5d9253606a3
Benny Degezelle <benny@gorilla-webdesign.be>
2008-06-06 19:51:50 +0800

    removing debugger call..
    Signed-off-by: Jan De Poorter <jan@defv.be>

--- lib/site_language/controller_extensions.rb
+++ lib/site_language/controller_extensions.rb
@@ -45,7 +45,6 @@ module SiteLanguage::ControllerExtensions
           response.headers.delete('Cache-Control')
           url = params[:url].to_s
           lang = params[:language].to_s
-          debugger
           if (request.get? || request.head?) and live? and (@cache.response_cached?(lang + '-' + url))
             @cache.update_response(lang + '-' + url, response, request)
             @performed_render = true
